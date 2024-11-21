922a2bd419d8314986c4596e43990e61fcc8da7a
Sathvik-Chowdary-Veerapaneni <sathvikchowdaryveerapaneni@gmail.com>
2024-11-21 10:58:30 -0600

    TMDB API Client Class Creation
    - Intialized base_url and headers for json and Bearer authorization
    - Loaded api_key from .env using dotenv lib

---
+++ Data_Collection/Movie_Data_Collection_by_Year_Pipeline.py
@@ -0,0 +1,19 @@
+#Importing Required Librarires
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key=os.getenv('TMDB_BEARER_TOKEN')
+
+
+class TMDB_API_Client:
+    ''' Managing TMDB API Interactions and requests '''
+
+    def __init__(self, API_KEY:str):
+        self.base_url = "https://api.themoviedb.org/3"
+        self.headers = {
+            "accept":"applicaiton/json",
+            "Authorization":f"Bearer {API_KEY}"
+        }
+
+client = TMDB_API_Client(api_key)
