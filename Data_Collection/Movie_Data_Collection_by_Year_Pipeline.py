955f65c940bc52de920872c16f87c071b8db4977
Sathvik-Chowdary-Veerapaneni <sathvikchowdaryveerapaneni@gmail.com>
2024-11-21 11:21:19 -0600

    Testing API

--- Data_Collection/Movie_Data_Collection_by_Year_Pipeline.py
+++ Data_Collection/Movie_Data_Collection_by_Year_Pipeline.py
@@ -12,8 +12,8 @@ class TMDB_API_Client:
     def __init__(self, API_KEY:str):
         self.base_url = "https://api.themoviedb.org/3"
         self.headers = {
-            "accept":"applicaiton/json",
-            "Authorization":f"Bearer {API_KEY}"
+            "accept": "applicaiton/json",
+            "Authorization": f"Bearer {API_KEY}"
         }
 
 client = TMDB_API_Client(api_key)
